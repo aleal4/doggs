a6572ad91b173e09da94447a559850d7db3a0892
Augusto Leal <aleal4@gmail.com>
2022-03-29 16:09:25 -0700

    footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import styles from './Footer.module.css';
+import { ReactComponent as Dogs } from '../Assets/dogs-footer.svg';
 
 const Footer = () => {
-  return <div className={styles.footer}></div>;
+  return (
+    <footer className={styles.footer}>
+      <Dogs />
+      <p>Doggs. All Rights Reserved</p>
+    </footer>
+  );
 };
 
 export default Footer;
